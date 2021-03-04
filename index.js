bed814495a4c482ef8ac2c0450609b5a131bb8dc
chaicopadillag <chaicopadillag@gmail.com>
2021-03-04 10:58:59 -0500

    Agregando Routes Refrech Canot

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { json } = require('express');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const path = require('path');
 const { dbConection } = require('./database/configdb');
+
 
 // console.log(process.env);
 const app = express();
@@ -12,6 +14,11 @@ dbConection();
 // TODO: Cors
 app.use(cors());
 app.use(express.static('public'));
+
+app.get('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname+'/public/index.html' ) );
+});
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
