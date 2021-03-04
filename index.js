b165567ad57cc6218d38ae05285216085396ffcb
chaicopadillag <chaicopadillag@gmail.com>
2021-03-04 11:05:59 -0500

    ruta refresh

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ dbConection();
 app.use(cors());
 app.use(express.static('public'));
 
-app.get('*', ( req, res ) => {
-    res.sendFile( path.join( __dirname+'/public/index.html' ) );
-});
-
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.get('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname+'/public/index.html' ) );
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Servidor corriendo en Puerto: ${process.env.PORT}`);
